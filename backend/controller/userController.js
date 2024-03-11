e9fa0a154e6ea0b6e7524d127bfc89b9b4196147
Himanshu Bajpai <himanshubajpai0644@gmail.com>
2024-03-12 01:38:27 +0530

    reset password url modified

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -101,7 +101,7 @@ exports.forgetPasswordController = async (req,res,next) => {
         await user.save({validateBeforeSave:false});
 
         // generating url for reset password
-        const resetPasswordURL = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+        const resetPasswordURL = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;
 
         const message = `your reset Password Token is: \n\n ${resetPasswordURL} \n\n if you have not requested for this email, then please ignore it.`
     
